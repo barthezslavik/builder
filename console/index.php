d242fbf292020fe43c3d395a9bfec816497fb8a1
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-01-18 14:28:34 +0200

    console output

--- console/index.php
+++ console/index.php
@@ -4,11 +4,30 @@ ini_set('display_errors', "on");
 error_reporting(E_ALL);
 
 class Console {
+  
+  function __construct() {
+    $this->output_file = "/tmp/console_output";
+  }
+
   function parse_params($post) {
+    file_put_contents($this->output_file, print_r($post));
+    $this->params = $post;
+  }
+
+  function run() {
+    print'<pre>';
+    print_r($this->params);
+    print'</pre>';
+    die("+++");    
   }
 
   function render_css() {
-    ?><style>input { width:1200px; height:40px; font-size: 18px; font-family: Monaco}</style><?
+    ?>
+    <style>
+      input { width:1200px; height:40px; font-size: 18px; font-family: Monaco} 
+      #screen { width:1200px; height:600px; font-size: 18px; font-family: Monaco} 
+    </style>
+    <?
   }
 
   function render_js() {
@@ -16,7 +35,12 @@ class Console {
   }
 
   function render_view() {
-    ?><input type="text" name="command" id="console"><?
+    $output = file_get_contents($this->output_file);
+    ?><div id="screen"><?=$output ?></div>
+      <form method="post">
+      <input type="text" name="command" id="console">
+      </form>
+    <?
   }
 }
 
@@ -25,4 +49,5 @@ $console->render_view();
 $console->render_css();
 $console->render_js();
 $console->parse_params($_POST);
+$console->run();
 
