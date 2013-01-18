f923c86152f9a1b845519361a4b43ebe7e5769ae
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-01-18 10:42:27 +0200

    make style

--- console/index.php
+++ console/index.php
@@ -8,16 +8,21 @@ class Console {
   }
 
   function render_css() {
-    ?><style>input {width:300px;}</style><?
+    ?><style>input { width:1200px; height:40px; font-size: 18px; font-family: Monaco}</style><?
+  }
+
+  function render_js() {
+    ?><script type="text/javascript">document.getElementById("console").focus();</script><?
   }
 
   function render_view() {
-    ?><input type="text" name="command"><?
+    ?><input type="text" name="command" id="console"><?
   }
 }
 
 $console = new Console();
 $console->render_view();
 $console->render_css();
+$console->render_js();
 $console->parse_params($_POST);
 
