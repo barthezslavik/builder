202535a4dfe43928b9e02b34c5ec80036e8c8c6c
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-01-20 01:00:36 +0200

    try fix

--- console/console.js
+++ console/console.js
@@ -1,41 +1,37 @@
 $(document).ready(function() {
-  variants = [
-  "g scaffold", "g model", "g controller",
-]
+  variants = ["g scaffold", "g model", "g controller"]
   $("#console").focus();
 
-var history = $("#screen").html().split("<br>");
-history = history.slice(0, history.length-1);
-var cursor = history.length;
+  var history = $("#screen").html().split("<br>");
+  history = history.slice(0, history.length-1);
+  var cursor = history.length;
 
-$("#console").keyup(function(e) {
-  console.log(e.keyCode);
-  switch(e.keyCode) {
-    case 8: {
-      variants = $("#autocomplete").find("div");
-      console.log(variants);
-    }
+  $("#console").keyup(function(e) {
+    console.log(e.keyCode);
+    switch(e.keyCode) {
+      case 8: {
+      }
 
-    case 40: {
-      variants = $("#autocomplete").find("div");
-      console.log(variants);
-    }
+      case 40: {
+        variants = $("#autocomplete").find("div");
+        console.log(variants);
+      }
 
-    case 38: {
-      if (cursor > 0) { 
-        cursor--;
-        $(this).val(history[cursor]);
+      case 38: {
+        if (cursor > 0) { 
+          cursor--;
+          $(this).val(history[cursor]);
+        }
+      }
+
+      default: {
+        var current_value = $("#console").val();
+        $.each(variants, function(index, value) {
+          if (value.match(current_value)) {
+            console.log(value);
+          }
+        });
       }
     }
-
-    default: {
-      var current_value = $("#console").val();
-      $.each(variants, function(index, value) {
-        if (value.match(current_value)) {
-          console.log(value);
-        }
-      });
-    }
-  }
-});
+  });
 });
