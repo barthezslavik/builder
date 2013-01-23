c6e0bf39b576c5ed86a4b2716740297875daebb5
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-01-23 17:55:51 +0200

    check create works

--- lib/Migration.php
+++ lib/Migration.php
@@ -8,28 +8,27 @@ class Migration extends Singleton {
   }
 
   function create_table() {
-    
-  }
-
-  function drop_table() {
-  }
-
-}
-
-  /*static function execute() {
-    $columns = "`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY";
+    $data = func_get_args();
+    $table_name = $data[0];
+    $fields = array_slice($data,1);
+    $query = "`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY";
     foreach($fields as $value) {
       $info = explode(":",$value);
       if ($info) {
-        if($info[1] == "string") { $columns .= ", `{$info[0]}` VARCHAR( 255 ) NOT NULL"; }
-          if($info[1] == "text") { $columns .= ", `{$info[0]}` TEXT NOT NULL"; }
-            if($info[1] == "integer") { $columns .= ", `{$info[0]}` INTEGER"; }
-              if($info[1] == "boolean") { $columns .= ", `{$info[0]}` TINYINT(1)"; }
+        if($info[0] == "string") { $query .= ", `{$info[1]}` VARCHAR( 255 ) NOT NULL"; }
+        if($info[0] == "text") { $query .= ", `{$info[1]}` TEXT NOT NULL"; }
+        if($info[0] == "integer") { $query .= ", `{$info[1]}` INTEGER"; }
+        if($info[0] == "boolean") { $query .= ", `{$info[1]}` TINYINT(1)"; }
+        if($info[0] == "datetime") { $query .= ", `created_at` DATETIME, `updated_at` DATETIME"; }
       }
     }
 
-    $this->connection->query(
-      "CREATE TABLE IF NOT EXISTS `{$table_name}` ({$columns})
-      ");
-  }*/
+    $this->connection->query("CREATE TABLE IF NOT EXISTS `{$table_name}` ({$query})");
+  }
+
+  function drop_table() {
+    $this->connection->query("DROP TABLE `samples`");
+  }
+
+}
 
