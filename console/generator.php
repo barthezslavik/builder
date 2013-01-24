e9930ee680740f51c4c92c0d74e0efa50443ccb9
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-01-24 12:34:58 +0200

    fix extension and name

--- console/generator.php
+++ console/generator.php
@@ -5,14 +5,18 @@ error_reporting(E_ALL);
 
 class Generator {
   function create_migration($params) {
-    $lines = explode("\n",file_get_contents("console/templates/scaffold/migration"));
-    $table = $params[0];
+    $lines = explode("\n",file_get_contents("console/templates/scaffold/migration.php"));
+    $table = $params[1];
     $columns_with_types = '"string:name", "text:description"';
     foreach($lines as $num => $line) {
       $lines[$num] = str_replace("{table}",$table, $lines[$num]);
       $lines[$num] = str_replace("{columns_with_types}",$columns_with_types, $lines[$num]);
       $lines[$num] .= "\n"; 
     } 
+    print'<pre>';
+    print_r($lines);
+    print'</pre>';
+    die("+++");
     $file_name = "db/migrate/".date('Ydmhis')."_create_{$table}.php";
     file_put_contents($file_name, $lines);
     chmod($file_name,0777);
---
+++ console/templates/scaffold/migration.php
@@ -0,0 +1,10 @@
+<?php
+class Create{table} extends ActiveRecord\Migration {
+  function up() {
+    $this->create_table("{table}", "{columns_with_types}", "datetime");
+  }
+
+  function down() {
+    $this->drop_table("{table}");
+  }
+}
